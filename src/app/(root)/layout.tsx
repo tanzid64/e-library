305160c792d0f207dbf7d897e25a11ef940d8e37
tanzid64 <tanzid3@gmail.com>
2025-01-12 23:44:01 +0600

    feat: Avatar added to header for authenticated user

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -14,7 +14,7 @@ const RootLayout: FC<RootLayoutProps> = async ({ children }) => {
   return (
     <main className="root-container">
       <div className="mx-auto max-w-7xl">
-        <Header />
+        <Header session={session} />
         <div className="mt-20 pb-20">{children}</div>
       </div>
     </main>
--- src/components/global/header/index.tsx
+++ src/components/global/header/index.tsx
@@ -1,17 +1,23 @@
 "use client";
-import { cn } from "@/lib/utils";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { cn, getInitials } from "@/lib/utils";
+import { Session } from "next-auth";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { FC } from "react";
 
-export const Header: FC = () => {
+interface HeaderProps {
+  session: Session;
+}
+
+export const Header: FC<HeaderProps> = ({ session }) => {
   const pathname = usePathname();
 
   return (
     <header className="my-10 flex justify-between gap-5">
       <Link href={"/"} className="">
-        <Image src={'/icons/logo.svg'} alt="logo" width={40} height={40} />
+        <Image src={"/icons/logo.svg"} alt="logo" width={40} height={40} />
       </Link>
       <ul className="flex flex-row items-center gap-8">
         <li>
@@ -23,6 +29,16 @@ export const Header: FC = () => {
             )}
           >
             Library
+          </Link>
+        </li>
+
+        <li>
+          <Link href="/me">
+            <Avatar>
+              <AvatarFallback className="bg-amber-100">
+                {getInitials(session?.user?.name || "IN")}
+              </AvatarFallback>
+            </Avatar>
           </Link>
         </li>
       </ul>
